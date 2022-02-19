0ed93b7c6e4469e3cba974a32d78eaffe3a27b50
Konstantin Kiritsenko <k@componentk.com>
2022-02-19 18:13:15 +0300

    SGSW6-119 install default css

--- src/ShopgateConnectSW6.php
+++ src/ShopgateConnectSW6.php
@@ -5,7 +5,22 @@ declare(strict_types=1);
 namespace Shopgate\ConnectSW6;
 
 use Shopware\Core\Framework\Plugin;
+use Shopware\Core\Framework\Plugin\Context\InstallContext;
+use Shopware\Core\System\SystemConfig\SystemConfigService;
 
 class ShopgateConnectSW6 extends Plugin
 {
+    public function install(InstallContext $installContext): void
+    {
+        /** @var SystemConfigService $configBridge */
+        $configBridge = $this->container->get(SystemConfigService::class);
+        $configBridge->set(Config::KEY_CSS, $this->getDefaultCss());
+    }
+
+    private function getDefaultCss(): string
+    {
+        return "
+            .test { padding: 0; };
+        ";
+    }
 }
