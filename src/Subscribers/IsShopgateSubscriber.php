a0a1b79ca146af6ca74d5f47f2e125d62b1b8e08
Konstantin Kiritsenko <k@componentk.com>
2022-03-18 15:26:54 +0300

    SGSW6-115 Developer cookie added

--- src/Subscribers/IsShopgateSubscriber.php
+++ src/Subscribers/IsShopgateSubscriber.php
@@ -7,6 +7,7 @@ namespace Shopgate\ConnectSW6\Subscribers;
 use Shopware\Core\Framework\Struct\ArrayStruct;
 use Shopware\Storefront\Pagelet\Header\HeaderPageletLoadedEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class IsShopgateSubscriber implements EventSubscriberInterface
 {
@@ -21,17 +22,29 @@ class IsShopgateSubscriber implements EventSubscriberInterface
 
     public function addIsShopgate(HeaderPageletLoadedEvent $event): void
     {
-        $hasSession = $event->getRequest()->hasSession();
+        $sgCookie = $this->handleDevelopmentCookie($event->getRequest());
         $sgAgent = strpos($event->getRequest()->headers->get('User-Agent'), 'libshopgate') !== false;
+        $hasSession = $event->getRequest()->hasSession();
         $sgSession = $hasSession && $event->getRequest()->getSession()->get(self::SG_SESSION_KEY, 0);
-        $sgCookie = $event->getRequest()->cookies->get(self::SG_SESSION_KEY, 0);
 
         $isShopgate = $sgAgent || $sgSession || $sgCookie;
         if (!$isShopgate) {
             return;
         }
         $event->getRequest()->getSession()->set(self::SG_SESSION_KEY, 1);
-        $event->getRequest()->cookies->set('sgWebView', 1);
         $event->getPagelet()->addExtension('sg_connect_data', new ArrayStruct());
+    }
+
+    /**
+     * Helper logic for developers to enable "mobile" call
+     * without needing the SG App. More in the README.md
+     */
+    private function handleDevelopmentCookie(Request $request): bool
+    {
+        $sgCookie = $request->cookies->get(self::SG_SESSION_KEY, false);
+        if ($sgCookie === '0' && $request->hasSession()) {
+            $request->getSession()->remove(self::SG_SESSION_KEY);
+        }
+        return (bool)$sgCookie;
     }
 }
