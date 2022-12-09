98ff0e2a978458b2c10af7f67c732bbbeeb7816c
Tristan Feldbusch <tristan.feldbusch@apite.io>
2022-12-09 10:49:12 +0100

    SW6M-64 Changed condition for webpack hot module reloading

--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -3,10 +3,10 @@ import SgWebcheckoutAppPlugin from './sg-webcheckout-app/sg-webcheckout-app.plug
 
 // Register your plugin via the existing PluginManager
 // see src/vendor/shopware/storefront/Resources/app/storefront/src/plugin-system/plugin.manager.js
-const PluginManager = window.PluginManager;
-PluginManager.register('SgWebcheckoutAppPlugin', SgWebcheckoutAppPlugin, '[data-sg-webcheckout-app-plugin]');
+    const PluginManager = window.PluginManager;
+    PluginManager.register('SgWebcheckoutAppPlugin', SgWebcheckoutAppPlugin, '[data-sg-webcheckout-app-plugin]');
 
 // Necessary for the webpack hot module reloading server
-if (module && module.hot) {
+if (module.hot) {
     module.hot.accept();
 }
